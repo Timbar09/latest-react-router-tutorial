83ce70a1d9e4da6e42acecc2d7fb135f012c2cee
Miles Mosweu <tmtmosweu@gmail.com>
2023-12-07 18:48:37 +0200

    Add Contact route and Contact component

--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,17 @@ import Root from './routes/root';
 import ErrorPage from './error-page';
 
 import './index.css';
+import Contact from './routes/contact';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Root />,
     errorElement: <ErrorPage />,
+  },
+  {
+    path: '/contacts/:contactId',
+    element: <Contact />,
   },
 ]);
 
