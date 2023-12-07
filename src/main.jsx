f17eb27b37596469697700b80d9d857aa7a25edd
Miles Mosweu <tmtmosweu@gmail.com>
2023-12-07 19:24:51 +0200

    Update Root component import and add loader to router configuration

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import Root from './routes/root';
+import Root, { loader as rootLoader } from './routes/root';
 import ErrorPage from './error-page';
 
 import './index.css';
@@ -13,6 +13,7 @@ const router = createBrowserRouter([
     path: '/',
     element: <Root />,
     errorElement: <ErrorPage />,
+    loader: rootLoader,
     children: [
       {
         path: 'contacts/:contactId',
